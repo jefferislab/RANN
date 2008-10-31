3e3b633de8deda6cfc931a3f259a5ff25844ae6c
Gregory Jefferis <jefferis@gmail.com>
2008-10-31 18:57:07 +0000

    whitespace - all initial spaces to tabs and CRLF->LF

--- R/nn.R
+++ R/nn.R
@@ -29,21 +29,21 @@ nn <- function(data, mask=rep.int(1, times=ncol(data)-1), p=min(10,nrow(d))
 		nn.idx   = integer(p*M),
 		nn       = double(p*M), PACKAGE="knnFinder")
 		
-    # now put the returned vector into an (M x p) array
-    nn.indexes  <- array(dim=c(M, p))
-    nn.dist     <- array(dim=c(M, p))
-    output.dist <- array(dim=c(M, p))
-    ptr        <- 1
-    for(i in 1 : M)
-    {
-        for(j in 1 : p)
-        {
-            nn.indexes[i,j]  <- results$nn.idx[ptr]
-            nn.dist[i,j]     <- (results$nn[ptr])
-            ptr <- ptr + 1
-        } # end inner for
-    } # end outer for
+	# now put the returned vector into an (M x p) array
+	nn.indexes  <- array(dim=c(M, p))
+	nn.dist     <- array(dim=c(M, p))
+	output.dist <- array(dim=c(M, p))
+	ptr        <- 1
+	for(i in 1 : M)
+	{
+		for(j in 1 : p)
+		{
+			nn.indexes[i,j]  <- results$nn.idx[ptr]
+			nn.dist[i,j]     <- (results$nn[ptr])
+			ptr <- ptr + 1
+		} # end inner for
+	} # end outer for
 
-    return(list(nn.idx=data.frame(nn.indexes), nn.dists=data.frame(nn.dist)))
+	return(list(nn.idx=data.frame(nn.indexes), nn.dists=data.frame(nn.dist)))
 }
 
--- src/NN.cc
+++ src/NN.cc
@@ -7,9 +7,9 @@
 //------------------------------------------------------------------------------------------------
 extern "C"
 {
-    void get_NN(double *data, int *mask, int *sumMask, int *k, int *dim, int *m_pts, int *nn_index,
+	void get_NN(double *data, int *mask, int *sumMask, int *k, int *dim, int *m_pts, int *nn_index,
 		double *distances)
-    {
+	{
 	int		d;		// Actual Dimension
 	int		M;		// Number of Data points
 	double		error_bound;	// enough said!
@@ -20,7 +20,7 @@ extern "C"
 	ANNdistArray	dists;		// Near neighbor distances
 	ANNkd_tree	*the_tree;	// Search structure
 
-    	d		= *dim;
+	d		= *dim;
 	M		= *m_pts;
 	numNN		= *k;
 	error_bound 	= 00.00;
@@ -37,57 +37,57 @@ extern "C"
 	// Next 2 for loops are concerned with getting the linear R array into the ANN format
 	for(int i = 0; i < d-1; i++)
 	{
-	    d_ptr[i] = 0;
-	    d_ptr[i] = i*M;
+		d_ptr[i] = 0;
+		d_ptr[i] = i*M;
 	} // end for
 
 	for(int i = 0; i < M; i++) // now construct the points
 	{
-	    ANNpoint p = new ANNcoord[*sumMask];
-	    int myJ = 0;
-	    for(int j = 0; j < d-1; j++)
-	    {
-		int temp = d_ptr[j];
-
-		if(mask[j] == 1)
+		ANNpoint p = new ANNcoord[*sumMask];
+		int myJ = 0;
+		for(int j = 0; j < d-1; j++)
 		{
-		    p[myJ] = 00.00;
-		    p[myJ] = data[temp];
-		    myJ++;
-		}
+			int temp = d_ptr[j];
 
-		d_ptr[j] = 0;
-		d_ptr[j] = temp + 1;
-	    } // end inner for loop
+			if(mask[j] == 1)
+			{
+				p[myJ] = 00.00;
+				p[myJ] = data[temp];
+				myJ++;
+			}
 
-	    data_pts[i] = p;
-	    ANNpoint o;
-	    o = new ANNcoord[1];
-	    o[0] = data[incOutputData];
-	    output_pts[i] = o;
-	    incOutputData++;
+			d_ptr[j] = 0;
+			d_ptr[j] = temp + 1;
+		} // end inner for loop
+
+		data_pts[i] = p;
+		ANNpoint o;
+		o = new ANNcoord[1];
+		o[0] = data[incOutputData];
+		output_pts[i] = o;
+		incOutputData++;
 	} // end for
 
 	the_tree = new ANNkd_tree(	// Build search structure
-		    data_pts,		// The data points
-		    M,			// Number of points
-		    *sumMask);		// Dimension of space
+			data_pts,		// The data points
+			M,			// Number of points
+			*sumMask);		// Dimension of space
 
 	for(int i = 0; i < M; i++)	// read query points
 	{
-	    the_tree->annkSearch(	// search
+		the_tree->annkSearch(	// search
 			data_pts[i],	// query point
 			numNN,		// number of near neighbors
 			nn_idx,		// nearest neighbors (returned)
 			dists,		// distance (returned)
 			error_bound);	// error bound
 
-	    for (int j = 0; j < numNN; j++)
-	    {
-	    	distances[ptr] = sqrt(dists[j]);	// unsquare distance
-		nn_index[ptr]  = nn_idx[j] + 1;			// put indexes in returned array
-		ptr++;
-	    } // end inner for
+		for (int j = 0; j < numNN; j++)
+		{
+			distances[ptr] = sqrt(dists[j]);	// unsquare distance
+			nn_index[ptr]  = nn_idx[j] + 1;			// put indexes in returned array
+			ptr++;
+		} // end inner for
 	} // end for
 
 	// Do a little bit of memory management......
@@ -96,6 +96,6 @@ extern "C"
 	delete nn_idx;
 	delete dists;
 	delete the_tree;
-    }
+	}
 }
 
