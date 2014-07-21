48c27e476d4381a75b2e777d0bc11f2ce8dac6cd
Gregory Jefferis <jefferis@gmail.com>
2014-07-21 11:33:34 +0100

    ANN: warning control reaches end of non-void function
    * when this function errors out and RANN is defined, annError is called
      with level ANNabort. This means that R's error function is called and
      execution stops, but the compiler cannot tell this.
    * therefore add a return NULL after annError to avoid this warning

--- src/kd_dump.cpp
+++ src/kd_dump.cpp
@@ -439,6 +439,7 @@ static ANNkd_ptr annReadTree(
 	}
 	else {
 		annError("Illegal node type in dump file", ANNabort);
+    return NULL;            // to keep the compiler happy
 #ifndef RANN
 		exit(0);								// to keep the compiler happy
 #endif
