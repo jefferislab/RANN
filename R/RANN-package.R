028e249e7881ac8dd230355695e34be01da3c01c
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-01-22 15:08:35 +0100

    rename package to RANN1

--- R/RANN-package.R
+++ R/RANN-package.R
@@ -1,7 +1,7 @@
 #' Wrapper for Arya and Mount's Approximate Nearest Neighbours (ANN) C++ library
 #' 
-#' @name RANN-package
-#' @aliases RANN
+#' @name RANN1-package
+#' @aliases RANN1
 #' @seealso \code{\link{nn2}}
 #' @docType package
 #' @keywords package
