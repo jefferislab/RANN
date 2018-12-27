811dc3e55e379c884c6b8a77f84a517e88650e71
Gregory Jefferis <jefferis@gmail.com>
2018-12-27 00:32:12 +0000

    Fix solaris build error
    * requested by BDR
    * closes #25

--- src/kd_split.cpp
+++ src/kd_split.cpp
@@ -31,7 +31,7 @@
 //	Constants
 //----------------------------------------------------------------------
 
-const double ERR = 0.001;				// a small value
+const double ANN_ERR = 0.001;				// a small value
 const double FS_ASPECT_RATIO = 3.0;		// maximum allowed aspect ratio
 										// in fair split. Must be >= 2.
 
@@ -95,7 +95,7 @@ void midpt_split(
 	ANNcoord max_spread = -1;			// find long side with most spread
 	for (d = 0; d < dim; d++) {
 										// is it among longest?
-		if (double(bnds.hi[d] - bnds.lo[d]) >= (1-ERR)*max_length) {
+		if (double(bnds.hi[d] - bnds.lo[d]) >= (1-ANN_ERR)*max_length) {
 										// compute its spread
 			ANNcoord spr = annSpread(pa, pidx, n, d);
 			if (spr > max_spread) {		// is it max so far?
@@ -165,7 +165,7 @@ void sl_midpt_split(
 	ANNcoord max_spread = -1;			// find long side with most spread
 	for (d = 0; d < dim; d++) {
 										// is it among longest?
-		if ((bnds.hi[d] - bnds.lo[d]) >= (1-ERR)*max_length) {
+		if ((bnds.hi[d] - bnds.lo[d]) >= (1-ANN_ERR)*max_length) {
 										// compute its spread
 			ANNcoord spr = annSpread(pa, pidx, n, d);
 			if (spr > max_spread) {		// is it max so far?
