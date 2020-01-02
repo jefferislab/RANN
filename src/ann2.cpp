751016b7a81f00793a6346869f61c5b9dfa50ec5
Kirill Müller <krlmlr+r@mailbox.org>
2020-01-02 08:18:02 +0100

    Add namespacing

--- src/ann2.cpp
+++ src/ann2.cpp
@@ -1,3 +1,16 @@
+#include <cstdlib>						// C standard lib defs
+#include <iomanip>				// I/O manipulators
+#include <cmath>			// math includes
+#include <iostream>			// I/O streams
+#include <cstring>			// C-style strings
+
+#ifdef RANN
+#include <R.h>				// R headers for error handling
+#endif
+
+
+namespace ann2 {
+
 #include "vendor/ann/ANN.cpp"
 #include "vendor/ann/bd_fix_rad_search.cpp"
 #include "vendor/ann/bd_pr_search.cpp"
@@ -11,3 +24,5 @@
 #include "vendor/ann/kd_split.cpp"
 #include "vendor/ann/kd_tree.cpp"
 #include "vendor/ann/kd_util.cpp"
+
+}
